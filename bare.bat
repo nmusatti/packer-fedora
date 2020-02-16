7d4381a147721c3ea085b1c4d6d5d2ac6b338f55
Nicola Musatti <nicola.musatti@gmail.com>
2020-02-16 20:23:59 +0100

    Correction

--- bare.bat
+++ bare.bat
@@ -1,2 +1,2 @@
-packer build -var-file windows.json -var-file bare.json centos.json
+packer build -var-file windows.json -var-file bare.json fedora.json
 IF %ERRORLEVEL% EQU 0 vagrant box add -f --name fedora-bare fedora-bare.box
