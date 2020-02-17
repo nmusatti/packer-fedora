d57505730f6e497b2e3c34da1986d8c75201bd93
Nicola Musatti <nicola.musatti@gmail.com>
2020-02-18 00:03:23 +0100

    Improved integration between GNOME and MATE

--- scripts/gui.sh
+++ scripts/gui.sh
@@ -1,4 +1,5 @@
 dnf update -y
-dnf group install -y gnome-desktop-environment
+#dnf group install -y gnome-desktop-environment
+dnf group install -y --allowerasing "Fedora Workstation"
 systemctl set-default graphical
 
--- scripts/mate.sh
+++ scripts/mate.sh
@@ -1,2 +1,3 @@
 dnf update -y
-dnf group install -y --allowerasing "MATE Desktop"
+#dnf group install -y --allowerasing "MATE Desktop"
+dnf group install -y --skip-broken "MATE Desktop"
